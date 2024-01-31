4fff3dd6f4c871d0ed5e8a71f5449b5bdcecb787
Raph2ll <raphael.ph2ll@gmail.com>
2024-01-31 16:19:02 -0300

    feat: ex02

---
+++ ex02/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+            Funcionario f2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+
+            Console.Write("Nome: ");
+            f1.nome = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            f1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário:");
+
+            Console.Write("Nome: ");
+            f2.nome = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            f2.salario = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Salário médio = {(f1.salario + f2.salario) / 2}");
+        }
+    }
+
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+
+    }
+
+}

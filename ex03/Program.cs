0923322b308fb2c3e85a6b6aca7a5d8b4f47e239
Raph2ll <raphael.ph2ll@gmail.com>
2024-02-01 13:31:57 -0300

    feat: ex03

---
+++ ex03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+
+            r1.width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            r1.height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Area = {r1.Area().ToString("F2", CultureInfo.InvariantCulture):2F}");
+            Console.WriteLine($"Perímetro = {r1.Perimeter().ToString("F1", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Diagonal = {r1.Diagonal().ToString("F2", CultureInfo.InvariantCulture)}");
+
+
+        }
+    }
+    class Rectangle
+    {
+        public double width;
+        public double height;
+
+        public double Area()
+        {
+            double ret = width * height;
+            return ret;
+        }
+        public double Perimeter()
+        {
+            double ret = 2 * (width + height);
+            return ret;
+        }
+        public double Diagonal(){
+            double ret = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(height, 2));
+            return ret;
+        }
+
+    }
+}
---
+++ ex03/readme.md
@@ -0,0 +1,12 @@
+
+EXERCÍCIO 03:
+Fazer um programa para ler os valores da largura e altura de um retângulo. Em
+seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar uma classe
+como mostrado no projeto ao lado.
+Exemplo:
+    Entre a largura e altura do retângulo:
+    3.00
+    4.00
+    AREA = 12.00
+    PERÍMETRO = 14.00
+    DIAGONAL = 5.00

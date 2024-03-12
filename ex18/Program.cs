2366571560070b9678d2b11b308f1f2fa658e5a5
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-12 14:42:35 -0300

    feeat: ex18

---
+++ ex18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            Console.WriteLine("How many students for course A? ");
+            int numA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numA; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                set.Add(student);
+            }
+
+            Console.WriteLine("How many students for course C? ");
+            int numB = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numB; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                set.Add(student);
+            }
+
+            Console.WriteLine("How many students for course B? ");
+            int numC = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numC; i++)
+            {
+                int student = int.Parse(Console.ReadLine());
+                set.Add(student);
+            }
+            
+            Console.WriteLine($"Total users: {set.Count}");
+        }
+    }
+}
